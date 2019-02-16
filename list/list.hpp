02577d4c0ffdfa0b08503b1746085730e531c332
fsZhuangB <617670307@qq.com>
2019-02-16 21:40:23 +0800

    add some api

--- list/list.hpp
+++ list/list.hpp
@@ -5,4 +5,26 @@
 template <class T>
 class List {
 
+    private:
+    // 头尾哨兵节点
+    ListNode * header;
+    ListNode * trailer;
+
+    public:
+    List();
+    ~List();
+
+    // 重载[]接口
+    T& operator[] (Rank r) const;
+
+    Rank size() const;
+    bool empty() const;
+
+    ListNode * first() const;
+    ListNode * last() const;
+    
+
+
+
+
 };
