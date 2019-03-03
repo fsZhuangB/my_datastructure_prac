2d13eab5a18b0d22b7b24eed95c2a2b6989969aa
fsZhuangB <617670307@qq.com>
2019-03-03 22:57:11 +0800

    dating with her today

--- list/doubly linked list/listNode.hpp
+++ list/doubly linked list/listNode.hpp
@@ -1,8 +1,14 @@
-typedef Rank int
+typedef int Rank;
 
 template <typename T>
 class ListNode
 {
+    T data;
+    ListNode<T>* pre;
+    ListNode<T>* succ;
 
-    
+    ListNode() {};
+    ListNode(T e, ListNode<T>* p = nullptr, ListNode<T>* s = nullptr)
+    : data(e), pre(p), succ(s) { }
+
 };
