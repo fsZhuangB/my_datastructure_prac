248fa2c2194564276969260f713ff751ab9a1de5
fsZhuangB <617670307@qq.com>
2019-03-13 20:11:59 +0800

    new update

---
+++ list/doubly_linked_list/build/CMakeFiles/list2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list2.dir/Users/fszhuangb/Documents/my_datastructure_prac/list/doubly_linked_list/src/main.cpp.o"
+  "list2.pdb"
+  "list2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/list2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
